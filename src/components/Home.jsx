430c48b4798abd9b88244a017af54986b107f23c
Samuel Adams <samueladams@outlook.co.nz>
2023-05-02 16:35:08 +1200

    add hashrouter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
+import Sam from "../assets/me.jpg"
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-      <img className='h-auto w-64 mb-6 rounded-full self-center mt-2' src='/me.jpg' alt='profileimage' />
+      <img className='h-auto w-64 mb-6 rounded-full self-center mt-2' src={Sam} alt='profileimage' />
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Samuel Adams,
